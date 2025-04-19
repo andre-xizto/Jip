df5e108f5a959dd432f94e2b9e13cba7a8dd8457
Andre <dmastergame74@gmail.com>
2025-04-19 10:12:27 -0300

    first commit

---
+++ src/main/java/dev/buskopan/jip/JIpApplication.java
@@ -0,0 +1,13 @@
+package dev.buskopan.jip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JIpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JIpApplication.class, args);
+    }
+
+}
